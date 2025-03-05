2865775e3b3cd19f2ca170ddd1c223f306c98ce9
Pramada Rao <pramadarao8@gmail.com>
2025-03-05 10:35:35 +0530

    Create app.js

---
+++ Task-management-frontend/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskDashboard from "./TaskDashboard";
+
+function App() {
+  return (
+    <div>
+      <TaskDashboard />
+    </div>
+  );
+}
+
+export default App;
